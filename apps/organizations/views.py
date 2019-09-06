39041eb16fc2d83492ecaa0a15d1686fa734e6db
HaibinZhong <1965063428@qq.com>
2019-09-06 16:03:22 +0800

    organizations 通过机构类别和所在城市对课程机构进行筛选

--- apps/organizations/views.py
+++ apps/organizations/views.py
@@ -10,8 +10,21 @@ from apps.organizations.models import CourseOrg, City
 class OrgView(View):
     def get(self, request, *args, **kwargs):
         all_org = CourseOrg.objects.all()
-        org_nums = CourseOrg.objects.count()
         all_citys = City.objects.all()
+
+        # 通过机构类别对课程机构进行筛选
+        category = request.GET.get("ct", '')
+
+        if category:
+            all_org = all_org.filter(category=category)
+
+        # 通过所在城市对课程机构进行筛选
+        city_id = request.GET.get('city', '')
+        if city_id:
+            if city_id.isdigit():
+                all_org = all_org.filter(city_id=int(city_id))
+
+        org_nums = all_org.count()
 
         # 对课程机构数据进行分页
         try:
@@ -26,4 +39,6 @@ class OrgView(View):
             'all_org': orgs,
             'org_nums': org_nums,
             'all_citys': all_citys,
+            'category': category,
+            'city_id': city_id,
         })
